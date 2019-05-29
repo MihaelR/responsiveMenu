08637de7b40358744940d2f7f2e0aabcaf895721
mihael <you@example.com>
2019-05-29 19:39:19 +0200

    css,js pt2

--- header.js
+++ header.js
@@ -18,8 +18,28 @@ $(document).ready(function() {
 
       if (target.length) {
           $('html, body').stop().animate({
-              scrollTop: target.offset().top -60
+              scrollTop: target.offset().top -60       //we donr want menu at caption, offset 60px
           }, 1000);
       }
 
   });
+
+  //RESPONSIVE MENU START
+
+  var body = $('body');
+  var menuTrigger = $('.js-menu-trigger');
+  var mainOverlay = $('.js-main-overlay');
+
+  menuTrigger.on('click', function(){
+    body.addClass('menu-is-active');
+  });
+
+  mainOverlay.on('click', function(){
+    body.removeClass('menu-is-active');
+  });
+
+  $('.menu li a').on('click', function(){
+      $('body').removeClass("menu-is-active");
+  });
+
+
